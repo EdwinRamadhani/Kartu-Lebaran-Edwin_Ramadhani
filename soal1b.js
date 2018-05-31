cff1750bf8f833f9e8f68107df999834fa215c2e
EdwinRamadhani <39075267+EdwinRamadhani@users.noreply.github.com>
2018-05-31 12:08:59 +0700

    Add files via upload

---
+++ soal1b.js
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+        <link href="https://fonts.googleapis.com/css?family=Courgette" rel="stylesheet"> 
+
+    <style>
+
+        h1{
+            font-family: 'Courgette'; font-size: 50px;
+        }
+
+        body{ 
+            background-color: yellow; text-align: center;
+        }
+        #content{
+            
+            display: grid; 
+            grid-template-columns: 80px 80px 80px; 
+            grid-template-rows: 80px 80px 80px;
+            max-width: 240px;
+            margin: 0 auto;
+            transform: rotate(45deg)
+        }
+        #content div{
+            background: green; padding: 30px;
+        }
+        #content div:nth-child(even){
+            background:greenyellow;
+        }
+    </style>
+</head>
+<body>
+<br>
+<br>
+<br>
+        <div id='content'>
+            <div class='kotak1'></div>
+            <div class='kotak2'></div>
+            <div class='kotak3'></div>
+            <div class='kotak4'></div>
+            <div class='kotak5'></div>
+            <div class='kotak6'></div>
+            <div class='kotak7'></div>
+            <div class='kotak8'></div>
+            <div class='kotak9'></div>
+        </div>
+        <br>
+        <br>
+        <br>
+        <p>
+            <h1>Selamat Hari Raya Idul Fitri</h1>
+<br>
+            <h1>Mohon Maaf Lahir dan Batin</h1>
+
+        </p>
+</body>
+</html>
